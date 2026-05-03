4ee99f39f0c4df759f08d2f9243d6ed57412eb9f
Egor <egor@MacBook-Air-Egor.local>
2026-05-03 11:56:02 +0300

    iterators

--- 04-concurrency/4.4-iterators/tasks/task02/main.go
+++ 04-concurrency/4.4-iterators/tasks/task02/main.go
@@ -27,6 +27,19 @@ import (
 // TODO: напиши функцию Fibonacci() iter.Seq[int]
 // Подсказка: используй две переменные a и b
 // Не забудь проверить возвращаемое значение yield!
+func Fibonacci() iter.Seq[int] {
+
+	return func(yield func(int) bool) {
+		a, b := 0, 1
+
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
 
 func main() {
 	count := 0
